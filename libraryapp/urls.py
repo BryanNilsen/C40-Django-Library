5023a39deffad8681ca94c25403f0c8bb90bf698
BryanNilsen <bryannilsen@gmail.com>
2020-08-06 10:27:50 -0500

    initial commit - completed through chapter 8

---
+++ libraryapp/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.urls import include
+from .views import *
+
+app_name = "libraryapp"
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('books/', book_list, name='books'),
+    path('books/<int:book_id>/', book_details, name='book'),
+    path('books/<int:book_id>/form/', book_edit_form, name='book_edit_form'),
+    path('librarians/', librarian_list, name='librarians'),
+    path('librarians/<int:librarian_id>/', librarian_details, name='librarian'),
+    path('libraries/', library_list, name='libraries'),
+    path('libraries/<int:library_id>/', library_details, name='library'),
+    path('library/form/', library_form, name='library_form'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', logout_user, name='logout'),
+    path('book/form/', book_form, name='book_form')
+]
